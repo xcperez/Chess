4fd6a6b76ce3c96370b2dd5e4651e7a74a18fabe
Xavier Perez <xcperez@hotmail.com>
2017-06-08 00:50:42 -0700

    Continued working on GameManager and Piece

--- Chess Game/src/Piece.java
+++ Chess Game/src/Piece.java
@@ -1,4 +1,30 @@
 
-public class Piece { // decorator
+public abstract class Piece { // decorator
+	
+	String piece = "Piece";
+	int xMove = 0;
+	int yMove = 0;
+	int [][] moveSet = new int [xMove][yMove];
+	boolean isDead = false;
+	
+	public String getPiece()
+	{
+		
+		return piece;
+		
+	}
+	public int [][] getMoveSet()
+	{
+		
+		return moveSet;
+		
+	}
+	void dead()
+	{
+		
+		isDead = true;
+		
+	}
+	
 
 }
